b24d5902fd186477872778c9851082ff77d68e48
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-17 17:10:16 -0700

    fixes test suite inside current module structure

--- src/binary01.rs
+++ src/binary01.rs
@@ -1,24 +1,23 @@
-mod binary01 {
-    pub fn vanilla_binary(list: &[usize], target: usize) -> Option<usize> {
-        //returns option Some with wrapped index of target
-        //or option None if not found
-        let mut left: usize = 0;
-        let mut right: usize = list.len() - 1;
 
-        let mut mid: usize = (right - left) / 2;
+pub fn vanilla_binary(list: &[usize], target: usize) -> Option<usize> {
+    //returns option Some with wrapped index of target
+    //or option None if not found
+    let mut left: usize = 0;
+    let mut right: usize = list.len() - 1;
 
-        while left <= right {
-            if list[mid] == target {
-                return Some(mid);
-            } else if list[mid] > target {
-                right = mid - 1;
-                mid = (right - left) / 2;
-            } else {
-                left = mid + 1;
-                mid = (right - left) / 2;
-            }
+    let mut mid: usize = (right - left) / 2;
+
+    while left <= right {
+        if list[mid] == target {
+            return Some(mid);
+        } else if list[mid] > target {
+            right = mid - 1;
+            mid = (right - left) / 2;
+        } else {
+            left = mid + 1;
+            mid = (right - left) / 2;
         }
-
-        None
     }
+
+    None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-pub mod binary01;
+mod binary01;
+mod binary01_tests;
 
 fn main() {
     println!("Hello, world!");
