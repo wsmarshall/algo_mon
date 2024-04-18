40eee40635a1f9171a7b303a8628fb2284d9df40
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-17 19:49:48 -0700

    gets vanilla binary tests to pass

--- src/binary01.rs
+++ src/binary01.rs
@@ -1,22 +1,30 @@
+use std::cmp::Ordering::*;
 
 pub fn vanilla_binary(list: &[usize], target: usize) -> Option<usize> {
     //returns option Some with wrapped index of target
     //or option None if not found
     let mut left: usize = 0;
+    if list.is_empty() {
+        //accounting for the empty array
+        return None;
+    }
     let mut right: usize = list.len() - 1;
 
-    let mut mid: usize = (right - left) / 2;
+    let mut mid: usize = (right + left) / 2;
 
     while left <= right {
-        if list[mid] == target {
-            return Some(mid);
-        } else if list[mid] > target {
-            right = mid - 1;
-            mid = (right - left) / 2;
-        } else {
-            left = mid + 1;
-            mid = (right - left) / 2;
+        match list[mid].cmp(&target) {
+            Equal => return Some(mid),
+            Less => left = mid + 1,
+            Greater => {
+                if right < 1 {
+                    break;
+                } else {
+                    right = mid - 1
+                }
+            }
         }
+        mid = (left + right) / 2;
     }
 
     None
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod binary01;
 mod binary01_tests;
 
+use crate::binary01::vanilla_binary;
+
 fn main() {
-    println!("Hello, world!");
+    let list: [usize; 7] = [1, 2, 3, 4, 5, 6, 7];
+    let target: usize = 0;
+
+    //should return None
+    assert_eq!(vanilla_binary(&list, target), None);
 }
